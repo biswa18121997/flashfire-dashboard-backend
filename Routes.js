febe9e66345df5605c0681015c4023b96c4abd5c
support1122 <support@flashfirejobs.com>
2025-09-22 16:41:43 +0530

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -119,6 +119,15 @@ app.post('/extension/login', ExtensionLogin);
 app.post('/extension/sendData', ReciveData);
 app.post('/extension/saveToDashboard', saveToDashboard);
 app.post('/extension/clientLogin', ClientLogin);
+app.get('/flash-fill', async (req, res) => {
+    try {
+      let profiles =await  ProfileModel.find().lean();
+      res.status(200).json(profiles);
+    } catch (error) {
+      console.log(error);
+    }
+    
+  })
 
 // Dashboard Manager routes
 app.get('/dashboard-managers', getDashboardManagers);
